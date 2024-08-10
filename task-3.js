f4db410942e033169d7385f6baf571ef1d59ca71
daria_hk <honcharuk.daria95@gmail.com>
2024-08-10 22:55:32 +0200

    code update

--- task-3.js
+++ task-3.js
@@ -1,13 +1,13 @@
 function getElementWidth(content, padding, border) {
     let contentNumber = parseInt(content);
-    let paddingNumber = parseInt(padding);
-    let borderNumber = parseInt(border);
+    let paddingNumber = parseInt(padding) * 2;
+    let borderNumber = parseFloat(border) * 2;
     let sum = contentNumber + paddingNumber + borderNumber;
     
     return `${sum} `;
     
 }
 console.log("Task3:");
-console.log(getElementWidth("50px", "8px", "4px")); // 62
-console.log(getElementWidth("60px", "12px", "8.5px")); // 80
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200

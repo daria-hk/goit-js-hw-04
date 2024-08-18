e5f7688a83ba20ad8d78c338eb69a12e1f5c77e7
daria_hk <honcharuk.daria95@gmail.com>
2024-08-18 16:30:44 +0200

    add files

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,11 @@
-function makeTransaction(quantity, pricePerDroid) {
-    return `You ordered: ${quantity} droids worth ${pricePerDroid*quantity} credits`;
+function slugify(title) {
+    let titletoLowerCase= title.toLowerCase();
+    let titleSplit = titletoLowerCase.split(" ");
+    let titleJoin = titleSplit.join("-");
+    return `${titleJoin}`;
 }
-console.log("Task1:");
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
+console.log('Task-1:');
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
